96917460801961592482fda1baa197d9ac50fa9b
harshpx <harsh.rzf@gmail.com>
2024-04-26 22:19:57 +0530

    geocodeing api changes

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -23,7 +23,7 @@ const SearchBar = () => {
     
     const searchHandler = (text)=>{
         if(!text) {text = 'New Delhi, New Delhi, Delhi, India'}
-        getPlaces(text)
+        getPlaces2(text)
         .then(res=>{
             setSearchResults(res)
             setOptions(getOptions(searchResults))
